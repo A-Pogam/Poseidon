c444028c78a387db08bbcfb71a5a7316d7b65ef8
A-Pogam <alicepogam59@hotmail.com>
2024-05-31 15:21:24 +0200

    uptd model classes

--- PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/model/Rating.java
+++ PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/model/Rating.java
@@ -1,10 +1,72 @@
 package com.poseidoncapitalsolution.trading.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "rating")
 public class Rating {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "moodysRating")
+    private String moodysRating;
+
+    @Column(name = "sandPRating")
+    private String sandPRating;
+
+    @Column(name = "fitchRating")
+    private String fitchRating;
+
+    @Column(name = "orderNumber")
+    private Integer orderNumber;
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMoodysRating() {
+        return moodysRating;
+    }
+
+    public void setMoodysRating(String moodysRating) {
+        this.moodysRating = moodysRating;
+    }
+
+    public String getSandPRating() {
+        return sandPRating;
+    }
+
+    public void setSandPRating(String sandPRating) {
+        this.sandPRating = sandPRating;
+    }
+
+    public String getFitchRating() {
+        return fitchRating;
+    }
+
+    public void setFitchRating(String fitchRating) {
+        this.fitchRating = fitchRating;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Integer orderNumber) {
+        this.orderNumber = orderNumber;
+    }
 }
--- PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/model/Rule.java
+++ PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/model/Rule.java
@@ -1,10 +1,77 @@
 package com.poseidoncapitalsolution.trading.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "rule")
 public class Rule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getSql_part() {
+        return sql_part;
+    }
+
+    public void setSql_part(String sql_part) {
+        this.sql_part = sql_part;
+    }
+
+    @Column(name = "json")
+    private String json;
+
+    @Column(name = "template")
+    private String template;
+
+    @Column(name = "sql_part")
+    private String sql_part;
 
 }
