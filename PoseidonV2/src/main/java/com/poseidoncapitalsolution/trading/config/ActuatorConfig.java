1e668590ea9ba814b144c23eff4f9e5f82fd85c2
A-Pogam <alicepogam59@hotmail.com>
2024-07-10 17:03:38 +0200

    added javadoc for Actuator

--- PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/config/ActuatorConfig.java
+++ PoseidonV2/src/main/java/com/poseidoncapitalsolution/trading/config/ActuatorConfig.java
@@ -5,8 +5,22 @@ import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeReposi
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for setting up Actuator-related beans.
+ * This configuration class is responsible for creating and managing
+ * the bean for HTTP trace repository which is used for tracing HTTP requests.
+ */
+
 @Configuration
 public class ActuatorConfig {
+
+	/**
+	 * Creates an in-memory HTTP exchange repository bean.
+	 * This repository stores HTTP request and response exchanges in memory.
+	 *
+	 * @return an instance of {@link InMemoryHttpExchangeRepository}
+	 */
+
 
 	@Bean
 	HttpExchangeRepository httpTraceRepository() {
